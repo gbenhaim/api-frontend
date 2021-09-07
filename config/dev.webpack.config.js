310905ebca86dd9bb7ac9142ac1fd4a3542a2221
Martin Maroši <marvusm.mmi@gmail.com>
2021-09-07 13:15:31 +0200

    Update all dependencies.

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,10 +1,24 @@
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
+
+const insightsProxy = {
+  https: false,
+  ...(process.env.BETA && { deployment: 'beta/apps' }),
+};
+
+const webpackProxy = {
+  deployment: process.env.BETA ? 'beta/apps' : 'apps',
+  useProxy: true,
+  useCloud: true,
+  env: 'ci-beta',
+  appUrl: process.env.BETA ? ['/beta/docs/api'] : ['/docs/api'],
+};
+
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
   debug: true,
-  https: true,
-  modules: ['apiDocs'],
+  sassPrefix: '.api-docs, .apiDocs',
+  ...(process.env.PROXY ? webpackProxy : insightsProxy),
 });
 
 const modulesConfig = require('@redhat-cloud-services/frontend-components-config/federated-modules')(
