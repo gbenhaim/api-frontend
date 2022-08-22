27fae93d75db5be5c3e61a2c5ca88519e92d84dd
Martin Maroši <marvusm.mmi@gmail.com>
2022-08-22 14:47:56 +0200

    Do not re-create the redux store instance

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { init, RegistryContext } from './store';
@@ -8,14 +8,15 @@ import PropTypes from 'prop-types';
 import logger from 'redux-logger';
 
 const AppEntry = ({ isDev }) => {
-  const registry = isDev ? init(logger) : init();
+  // cannot be in a render as it will create multiple store instances during a render and a race condition when accessing the context
+  const registry = useRef(isDev ? init(logger) : init());
   return (
     <RegistryContext.Provider
       value={{
-        getRegistry: () => registry,
+        getRegistry: () => registry.current,
       }}
     >
-      <Provider store={registry.getStore()}>
+      <Provider store={registry.current.getStore()}>
         <Router basename={getBaseName(window.location.pathname)}>
           <App />
         </Router>
